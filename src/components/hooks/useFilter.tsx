23894a8d45fd7b73fe2c4ab38b8bf0699995228f
Kulsuma03 <kulsumams72@gmail.com>
2022-12-30 18:48:11 +0600

    remove type

--- src/components/hooks/useFilter.tsx
+++ src/components/hooks/useFilter.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 
-export type User = {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    address: {};
-    phone: string;
-    website: string;
-    company: {};
 
-}
 
 const useFilter = (data: [], key: string) => {
  const newData = data.filter(value => value.name.includes(key)
